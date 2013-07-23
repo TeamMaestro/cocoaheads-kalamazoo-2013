efb0f48753962318d3a97bfc1bde993899750a09
Jason Anderson <jason@meetmaestro.com>
2013-07-23 12:44:47 -0400

    project files plus initial slide deck outline in readme doc

---
+++ MapKit/MapKit/MEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MEAppDelegate.h
+//  MapKit
+//
+//  Created by Jason Anderson on 7/23/13.
+//  Copyright (c) 2013 Maestro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
